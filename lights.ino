81ea4fdd4f0d28674026b36bc1e90b5f6786a5c1
rbaglio <rbaglio@bancroftschool.org>
2018-03-12 09:18:46 -0400

    Add files via upload

--- lights.ino
+++ lights.ino
@@ -1,15 +1,22 @@
+
 void setup() {
 pinMode(13, OUTPUT);
 pinMode(12, OUTPUT);
-
+pinMode(11, OUTPUT);
+pinMode(8, OUTPUT);
 }
 
   void loop() {
 digitalWrite(13, HIGH); 
-delay(1000);
+delay(750);
 digitalWrite(13, LOW);
 digitalWrite(12, HIGH);
-delay(1000);
+delay(750);
 digitalWrite(12, LOW);
-
-}
+digitalWrite(11, HIGH);
+delay(750);
+digitalWrite(11, LOW);
+digitalWrite(8, HIGH);
+delay(750);
+digitalWrite(8, LOW);
+ }
